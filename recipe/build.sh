8b53a124070451d4736a900fb023147a3ad5bba2
Isuru Fernando <isuruf@gmail.com>
2020-09-03 03:21:45 -0500

    Fix build error

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Get an updated config.sub and config.guess
+# Delete first because they have weird permissions
+rm -f config.sub
+rm -f config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 ./configure --prefix=$PREFIX \
